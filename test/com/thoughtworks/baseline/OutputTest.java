515ab09a931694d7bdf707e795d349412858ee8b
jhalaa <jhalaachinoy@gmail.com>
2015-09-19 11:23:59 +0530

    should return valid output pattern in first row and zeroth column after one tick

--- test/com/thoughtworks/baseline/OutputTest.java
+++ test/com/thoughtworks/baseline/OutputTest.java
@@ -23,6 +23,14 @@ public class OutputTest {
         assertEquals(output.patternAfterOneTick(array[0][1]),array1[0][1]);
     }
 
+    @Test
+    public void shouldReturnAValidOutputPatternInFirstRowAndZerothColumnAfterOneTickForABlockPattern() {
+        Output output= new Output();
+        String array[][] ={{"X","X"},{"X","X"}};
+        String array1[][] ={{"X","X"},{"X","X"}};
+        assertEquals(output.patternAfterOneTick(array[1][0]),array1[1][0]);
+    }
+
 
 
 }
