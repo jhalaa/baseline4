d12ec713502cc23473919930755e41765b516506
jhalaa <jhalaachinoy@gmail.com>
2015-09-19 13:26:04 +0530

    validator should return false if element cannot be accessed

--- test/com/thoughtworks/baseline/ValidatorTest.java
+++ test/com/thoughtworks/baseline/ValidatorTest.java
@@ -2,6 +2,7 @@ package com.thoughtworks.baseline;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ValidatorTest {
@@ -11,5 +12,12 @@ public class ValidatorTest {
         String array[][] ={{"X","X","X"},{"X","X","X"},{"X","X","X"}};
         Validator validator = new Validator(array);
         assertTrue(validator.validIndex(0,1));
+    }
+
+    @Test
+    public void shouldReturnFalseIfArrayElementCantbeAccessed(){
+        String array[][] ={{"X","X","X"},{"X","X","X"},{"X","X","X"}};
+        Validator validator = new Validator(array);
+        assertFalse(validator.validIndex(0, -1));
     }
 }
